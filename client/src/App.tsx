74257cc5ca51579ac85a10df5c2c08b9aad5df40
daniel1-xyz <danielkattan2@gmail.com>
2023-12-04 15:18:57 +0200

    fixed tours map

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { TourMap } from "./pages/ToursMap";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <TourMap />
     </div>
   );
--- client/src/pages/ToursMap.tsx
+++ client/src/pages/ToursMap.tsx
@@ -1,31 +1,37 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styled from "styled-components";
+import "leaflet/dist/leaflet.css";
 
-const positionForExample = [32, 34.8];
+const positionForExample = [32.08, 34.78];
 
 const TourMapWrapper = styled.div`
-  height: 100vh,
-  width: 100%
+  .full-height {
+    height: 100%;
+  }
+
+  height: 100%;
+  width: 100%;
 `;
 
 export const TourMap = () => {
   return (
-    <>
+    <TourMapWrapper>
       <MapContainer
-        center={positionForExample}
+        center={[positionForExample[0], positionForExample[1]]}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
+        className="full-height"
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={positionForExample}>
+        <Marker position={[positionForExample[0], positionForExample[1]]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
       </MapContainer>
-    </>
+    </TourMapWrapper>
   );
 };
