d88c48187956a6fac8e8dddc130de7777a7309c4
daniel1-xyz <danielkattan2@gmail.com>
2023-12-05 10:17:37 +0200

    minor fixes

--- client/src/pages/ToursMap.tsx
+++ client/src/pages/ToursMap.tsx
@@ -2,7 +2,10 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styled from "styled-components";
 import "leaflet/dist/leaflet.css";
 
-const positionForExample = [32.08, 34.78];
+const exampleCoords = {
+  lat: 32.08,
+  lon: 34.78,
+};
 
 const TourMapWrapper = styled.div`
   .full-height {
@@ -17,7 +20,7 @@ export const TourMap = () => {
   return (
     <TourMapWrapper>
       <MapContainer
-        center={[positionForExample[0], positionForExample[1]]}
+        center={[exampleCoords.lat, exampleCoords.lon]}
         zoom={13}
         scrollWheelZoom={true}
         className="full-height"
@@ -26,7 +29,7 @@ export const TourMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[positionForExample[0], positionForExample[1]]}>
+        <Marker position={[exampleCoords.lat, exampleCoords.lon]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
