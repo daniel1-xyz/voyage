42982d703d370d578cff0f9785cc0c7843084c1f
daniel1-xyz <danielkattan2@gmail.com>
2023-12-06 17:12:00 +0200

    installed nodemon and fixed app

--- server/app.ts
+++ server/app.ts
@@ -5,12 +5,12 @@ const db = require("./config/database.ts");
 
 db.authenticate()
   .then(() => console.log("DB Connected"))
-  .catch((err) => console.log("error" + err));
+  .catch((err: Error) => console.log("error" + err));
 
 const app = express();
 
-app.get("/", (req, res) => {});
+app.get("/", (req: Request, res: Response) => {});
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
