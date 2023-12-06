d09967d0646a6ad5987d247970ac62c7d065f9ae
daniel1-xyz <danielkattan2@gmail.com>
2023-12-06 15:33:48 +0200

    created sequelize db connection

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,1 +1,14 @@
-const sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
+
+// TEST, FIX LATER
+const sequelize = new Sequelize("postgres", "postgres", "Aa123456", {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
