8545399ab69a8312941989d61deacf1dba354e93
daniel1-xyz <danielkattan2@gmail.com>
2023-12-11 17:24:36 +0200

    added app route

--- server/routes/points.ts
+++ server/routes/points.ts
@@ -3,8 +3,6 @@ import { Point } from "../models/point";
 
 const router: Router = Router();
 
-router.get("/", (req: Request, res: Response) => {});
-
 router.get("/points", (req: Request, res: Response) => {
   res.send(Point.findAll());
 });
@@ -12,5 +10,7 @@ router.get("/points", (req: Request, res: Response) => {
 router.get("/point/:id", (req: Request, res: Response) => {
   res.send(Point.findByPk(req.params.id));
 });
+
+router.post("/point/new", (req: Request, res: Response) => {});
 
 export default router;
