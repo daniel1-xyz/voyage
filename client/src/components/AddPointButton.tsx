b869ebc7970592c0ae34c7ff15a4e76a7193dd68
daniel1-xyz <danielkattan2@gmail.com>
2023-12-24 16:26:52 +0200

    makeStyles to styled

--- client/src/components/AddPointButton.tsx
+++ client/src/components/AddPointButton.tsx
@@ -1,31 +1,29 @@
-import { IconButton } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import { IconButton, styled } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
-const useStyles = makeStyles({
-  addButton: {
-    position: "absolute",
-    bottom: "1%",
-    left: "1%",
-    height: "5rem",
-    width: "5rem",
-    fontSize: "3rem",
-    zIndex: 1000,
-    borderRadius: "100%",
-    backgroundColor: "#fff",
-    color: "#000",
-    "&:hover": {
-      backgroundColor: "#eee",
-    },
-    "&:focus": {
-      border: "none",
-      outline: "none",
-    },
+const AddButton = styled(IconButton)({
+  position: "absolute",
+  bottom: "1%",
+  left: "1%",
+  height: "5rem",
+  width: "5rem",
+  fontSize: "3rem",
+  zIndex: 1000,
+  borderRadius: "100%",
+  backgroundColor: "#fff",
+  color: "#000",
+  "&:hover": {
+    backgroundColor: "#eee",
   },
-  largerIcon: {
-    height: "2.5rem",
-    width: "2.5rem",
+  "&:focus": {
+    border: "none",
+    outline: "none",
   },
+});
+
+const LargerAddIcon = styled(AddIcon)({
+  height: "2.5rem",
+  width: "2.5rem",
 });
 
 export const AddPointButton = ({
@@ -33,14 +31,9 @@ export const AddPointButton = ({
 }: {
   openAddSidebar: () => void;
 }) => {
-  const classes = useStyles();
   return (
-    <IconButton
-      title="הוספת נקודה חדשה"
-      onClick={openAddSidebar}
-      className={classes.addButton}
-    >
-      <AddIcon className={classes.largerIcon} />
-    </IconButton>
+    <AddButton title="הוספת נקודה חדשה" onClick={openAddSidebar}>
+      <LargerAddIcon />
+    </AddButton>
   );
 };
