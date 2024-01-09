b34bf194b5b86d949c88c9e176a5384fb00279be
daniel1-xyz <danielkattan2@gmail.com>
2024-01-09 16:49:07 +0200

    added enhancer to store

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,7 +1,8 @@
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/rootReducer";
 import defaultState from "./constants/defaultState";
 import State from "./constants/stateInterface";
+import { thunk } from "redux-thunk";
 
 const rootState: State = {
   pointsToDisplay: defaultState.POINTS_TO_DISPLAY,
@@ -9,6 +10,8 @@ const rootState: State = {
   currentPoint: defaultState.CURRENT_POINT,
 };
 
-const store = createStore(rootReducer, rootState as Partial<State>);
+const enhancer = applyMiddleware(thunk);
+
+const store = createStore(rootReducer, rootState as Partial<State>, enhancer);
 
 export default store;
