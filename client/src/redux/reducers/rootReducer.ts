e27eadec19db58bdf59c75e523115eba86921df9
daniel1-xyz <danielkattan2@gmail.com>
2024-01-09 10:37:59 +0200

    fixed rootReducer caused error

--- client/src/redux/reducers/rootReducer.ts
+++ client/src/redux/reducers/rootReducer.ts
@@ -1,6 +1,4 @@
 import { combineReducers } from "redux";
 import { currentPointReducer } from "./currentPoint";
 
-export default combineReducers({
-  currentPointReducer,
-});
+export default combineReducers(currentPointReducer);
